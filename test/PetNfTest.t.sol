f8311a6b34a6149bfbd1ee39d6b01168c6b1a717
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-09-07 21:00:50 -0300

    Refactor PetNftTest to use state variables for addresses instead of constants

--- test/PetNfTest.t.sol
+++ test/PetNfTest.t.sol
@@ -7,72 +7,74 @@ import { PetNft } from '../src/PetNft.sol';
 contract PetNftTest is Test {
   PetNft private i_petNft;
 
-  address private constant BUYER = address(0x123);
-  address private constant OTHER_ADDRESS = address(0x456);
+  address private s_buyer;
+  address private s_otherAddress;
 
   function setUp() public {
     i_petNft = new PetNft();
+    s_buyer = makeAddr('buyer');
+    s_otherAddress = makeAddr('other');
   }
 
   function test_RequestPurchase() public payable {
-    vm.deal(BUYER, 1 ether);
+    vm.deal(s_buyer, 1 ether);
 
     vm.expectEmit(true, true, true, true);
-    emit PetNft.PurchaseRequested(BUYER, 1 ether, block.number);
+    emit PetNft.PurchaseRequested(s_buyer, 1 ether, block.number);
 
-    vm.prank(BUYER);
+    vm.prank(s_buyer);
     i_petNft.requestPurchase{ value: 1 ether }();
   }
 
   function testRevert_RequestPurchase_ZeroValue() public {
-    vm.prank(BUYER);
+    vm.prank(s_buyer);
     vm.expectRevert('Need ETH to purchase');
     i_petNft.requestPurchase();
   }
 
   function test_ApprovePurchase() public {
-    vm.deal(BUYER, 1 ether);
-    vm.prank(BUYER);
+    vm.deal(s_buyer, 1 ether);
+    vm.prank(s_buyer);
     i_petNft.requestPurchase{ value: 1 ether }();
 
     string memory uri = 'ipfs://some-uri';
 
     vm.expectEmit(true, true, true, true);
-    emit PetNft.PurchaseApproved(BUYER, 0);
+    emit PetNft.PurchaseApproved(s_buyer, 0);
 
     vm.prank(address(this)); // Owner approves
-    i_petNft.approvePurchase(BUYER, uri);
+    i_petNft.approvePurchase(s_buyer, uri);
 
-    assertEq(i_petNft.ownerOf(0), BUYER);
+    assertEq(i_petNft.ownerOf(0), s_buyer);
     assertEq(i_petNft.tokenURI(0), uri);
   }
 
   function testRevert_ApprovePurchase_NotOwner() public {
-    vm.prank(OTHER_ADDRESS);
+    vm.prank(s_otherAddress);
     vm.expectRevert(); // No specific error message for Ownable
-    i_petNft.approvePurchase(BUYER, '');
+    i_petNft.approvePurchase(s_buyer, '');
   }
 
   function test_RejectPurchase() public {
-    vm.deal(BUYER, 1 ether);
-    vm.prank(BUYER);
+    vm.deal(s_buyer, 1 ether);
+    vm.prank(s_buyer);
     i_petNft.requestPurchase{ value: 1 ether }();
 
     // After the purchase request, BUYER balance is 0 (sent 1 ether to contract)
-    uint256 buyerBalanceBefore = BUYER.balance; // This is 0
+    uint256 buyerBalanceBefore = s_buyer.balance; // This is 0
 
     vm.expectEmit(true, true, true, true);
-    emit PetNft.PurchaseRejected(BUYER, 1 ether);
+    emit PetNft.PurchaseRejected(s_buyer, 1 ether);
 
     vm.prank(address(this)); // Owner rejects
-    i_petNft.rejectPurchase(BUYER, 1 ether);
+    i_petNft.rejectPurchase(s_buyer, 1 ether);
 
-    assertEq(BUYER.balance, buyerBalanceBefore + 1 ether); // 0 + 1 ether = 1 ether
+    assertEq(s_buyer.balance, buyerBalanceBefore + 1 ether); // 0 + 1 ether = 1 ether
   }
 
   function testRevert_RejectPurchase_NotOwner() public {
-    vm.prank(OTHER_ADDRESS);
+    vm.prank(s_otherAddress);
     vm.expectRevert(); // No specific error message for Ownable
-    i_petNft.rejectPurchase(BUYER, 0);
+    i_petNft.rejectPurchase(s_buyer, 0);
   }
 }
