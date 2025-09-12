cd0b3297b6f8c2601aca91d97f93064d58a48ab6
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-09-12 00:09:41 -0300

    Update rainbowkit configuration to support new chains: anvil and baseSepolia.

--- web/src/lib/rainbowkitConfig.ts
+++ web/src/lib/rainbowkitConfig.ts
@@ -1,12 +1,12 @@
 'use client';
 
 import { http, createStorage, cookieStorage } from 'wagmi'
-import { sepolia, bscTestnet, blastSepolia } from 'wagmi/chains'
+import { anvil, baseSepolia } from 'wagmi/chains'
 import { type Chain, getDefaultConfig } from '@rainbow-me/rainbowkit'
 
 const projectId = '6a6bc9ccada2df8d6c64e6bc29a2abbe';
 
-const supportedChains: Chain[] = [sepolia, bscTestnet, blastSepolia];
+const supportedChains: Chain[] = [anvil, baseSepolia];
 
 export const rainbowkitConfig = getDefaultConfig({
     appName: 'WalletConnection',
