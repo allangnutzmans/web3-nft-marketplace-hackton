aa7d21db2a293174cf3d75f96419941804bf369c
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-09-16 23:08:48 -0300

    Add purchase router to main API for handling purchase-related requests

--- web/src/server/api/_root.ts
+++ web/src/server/api/_root.ts
@@ -1,6 +1,9 @@
 import { createTRPCRouter } from '@/server/trpc';
 import { nftRouter } from './routers/nft';
+import { purchaseRouter } from './routers/purchase';
+
 export const appRouter = createTRPCRouter({
     nft: nftRouter,
+    purchase: purchaseRouter,
 });
 export type AppRouter = typeof appRouter;
