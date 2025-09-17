f2982ae5869f2488a895befd343af2c23175f174
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-09-16 23:08:20 -0300

    Update rainbowkit configuration to include baseSepolia chain and adjust transport settings

--- web/src/lib/rainbowkitConfig.ts
+++ web/src/lib/rainbowkitConfig.ts
@@ -2,8 +2,10 @@
 
 import { http, createStorage, cookieStorage } from 'wagmi'
 import { defineChain } from 'viem'
+import { baseSepolia } from 'wagmi/chains'
+import { type Chain, getDefaultConfig } from '@rainbow-me/rainbowkit'
 
-// Define Anvil chain explicitly
+// Define Anvil chain explicitly - TODO FIX IT TO USE rainbowkit
 const anvilLocal = defineChain({
   id: 31337,
   name: 'Anvil Local',
@@ -18,21 +20,21 @@ const anvilLocal = defineChain({
     },
   },
 })
-import { type Chain, getDefaultConfig } from '@rainbow-me/rainbowkit'
 
 const projectId = '6a6bc9ccada2df8d6c64e6bc29a2abbe';
 
-const supportedChains: Chain[] = [anvilLocal];
+const supportedChains: Chain[] = [baseSepolia, anvilLocal]
 
 export const rainbowkitConfig = getDefaultConfig({
-    appName: 'Pet NFT Marketplace',
-    projectId,
-    chains: supportedChains as never,
-    ssr: false,
-    storage: createStorage({
-        storage: cookieStorage,
-    }),
-    transports: {
-        [anvilLocal.id]: http('http://127.0.0.1:8545')
-    }
-});
+  appName: 'Pet NFT Marketplace',
+  projectId,
+  chains: supportedChains as never,
+  ssr: false,
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  transports: {
+    [baseSepolia.id]: http(),
+    [anvilLocal.id]: http('http://127.0.0.1:8545')
+  },
+})
