dddcda45df590ca37e0fcbbb06fb1a549ce7cf0c
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-09-18 23:44:39 -0300

    Refactor rainbowkit configuration to conditionally include Anvil chain in development environment

--- web/src/lib/rainbowkitConfig.ts
+++ web/src/lib/rainbowkitConfig.ts
@@ -2,28 +2,22 @@
 
 import { http, createStorage, cookieStorage } from 'wagmi'
 import { defineChain } from 'viem'
-import { baseSepolia } from 'wagmi/chains'
+import { anvil, baseSepolia } from 'wagmi/chains'
 import { type Chain, getDefaultConfig } from '@rainbow-me/rainbowkit'
-
-// Define Anvil chain explicitly - TODO FIX IT TO USE rainbowkit
-const anvilLocal = defineChain({
-  id: 31337,
-  name: 'Anvil Local',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Ether',
-    symbol: 'ETH',
-  },
-  rpcUrls: {
-    default: {
-      http: ['http://127.0.0.1:8545'],
-    },
-  },
-})
 
 const projectId = '6a6bc9ccada2df8d6c64e6bc29a2abbe';
 
-const supportedChains: Chain[] = [baseSepolia, anvilLocal]
+const supportedChains: Chain[] = [baseSepolia]
+
+const transports: Record<number, ReturnType<typeof http>> = {
+  [baseSepolia.id]: http(),
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  supportedChains.push(anvil)
+  transports[anvil.id] = http('http://127.0.0.1:8545')
+}
+
 
 export const rainbowkitConfig = getDefaultConfig({
   appName: 'Pet NFT Marketplace',
@@ -33,8 +27,5 @@ export const rainbowkitConfig = getDefaultConfig({
   storage: createStorage({
     storage: cookieStorage,
   }),
-  transports: {
-    [baseSepolia.id]: http(),
-    [anvilLocal.id]: http('http://127.0.0.1:8545')
-  },
+  transports,
 })
