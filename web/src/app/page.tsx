3c0256d524105fc6a23c88d2d694f13f55a3cdf1
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-09-18 23:50:38 -0300

    Update Home page to integrate NFTCardTop for displaying top NFT and enhance layout with ETH price conversion for rare NFTs

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,10 +5,13 @@ import TiltedCard from "@/components/ui/tilted-card";
 import NFTMarketplaceCard from "@/components/ui/nft-marketplace-card";
 import { api } from "@/lib/trpc";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import NFTCardTop from "@/components/ui/nft-card-top";
 
 export default function Home() {
   const router = useRouter();
   const { data: nftsData, isLoading } = api.nft.list.useQuery();
+  const { data: ethPriceUSd, isLoading: isLoadingUsdPrice } = api.nft.getEthPriceUsd.useQuery();
+
 
   const topNft = nftsData?.[0];
   const rareNfts = nftsData || [];
@@ -19,7 +22,7 @@ export default function Home() {
 
   return (
     <div className="bg-white h-full overflow-auto">
-      <div className="relative w-full h-full flex items-center justify-center rounded-l-4xl min-h-[calc(100vh-8.5rem)] overflow-hidden">
+      <div className="relative w-full h-full flex items-center justify-center rounded-l-4xl min-h-[calc(100vh-9rem)] overflow-hidden">
         <div className="absolute inset-0 z-0">
           <Image
             src="/background.jpg"
@@ -33,21 +36,22 @@ export default function Home() {
 
         {/* Content with higher z-index */}
         <div className="relative z-10 w-full max-w-7xl mx-auto px-4">
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 min-h-[calc(100vh-10.5rem)]">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 min-h-[calc(100vh-9rem)]">
             {/* Top NFT */}
             <div className="lg:col-span-1">
               <div className="h-full flex flex-col pt-8">
                 <h2 className="text-xl font-semibold mb-4">Top NFT</h2>
                 <div className="flex-1 flex items-start justify-center lg:justify-start">
                   {topNft && (
-                    <TiltedCard
+                    <NFTCardTop
                       imageSrc={topNft.image}
                       altText={topNft.name}
                       captionText={topNft.name}
-                      containerHeight="300px"
-                      containerWidth="300px"
-                      imageHeight="300px"
-                      imageWidth="300px"
+                      name={topNft.name}
+                      description={topNft.description ?? topNft.name}
+                      ownedBy=""
+                      createdBy=""
+                      rarity="rare"
                     />
                   )}
                 </div>
@@ -56,33 +60,26 @@ export default function Home() {
 
             {/* Rare NFTs */}
             <div className="lg:col-span-2">
-              <div className="h-full flex flex-col pt-8">
-                <h2 className="text-xl font-semibold mb-4">Rare NFT</h2>
+              <div className="h-full flex flex-col lg:pt-8">
+                <h2 className="text-xl font-semibold mb-4 ms-22">SHOP NFT</h2>
                 <div className="flex-1">
-                  <ScrollArea className="h-full max-h-[calc(100vh-13.5rem)]">
+                  <ScrollArea className="h-full max-h-[calc(100vh-15rem)] mb-4">
                     {isLoading ? (
                       <p>Loading...</p>
                     ) : (
-                      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 pb-4">
+                      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 gap-y-4  max-w-2xl mx-auto">
                         {rareNfts.map((nft) => (
                           <NFTMarketplaceCard
                             key={nft.id}
                             name={nft.name}
                             imageSrc={nft.image}
+                            description={nft.description ?? nft.name}
                             altText={nft.description ?? nft.name}
-                            ownedBy={{
-                              name: "N/A",
-                              avatar:
-                                "https://via.placeholder.com/32/000000/ffffff?text=NA",
-                            }}
-                            createdBy={{
-                              name: "N/A",
-                              avatar:
-                                "https://via.placeholder.com/32/000000/ffffff?text=NA",
-                            }}
+                            ownedBy=""
+                            createdBy=""
                             price={{
                               eth: nft.price,
-                              usd: "",
+                              usd: ethPriceUSd ? (parseFloat(nft.price) * ethPriceUSd).toFixed(2) : "",
                             }}
                             onViewHistory={() =>
                               console.log("View history clicked for", nft.id)
